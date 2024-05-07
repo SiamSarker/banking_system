4b269a3ec723213a41ae6eaea9c7b94bd7499873
siamsarker <sarkersiam2@gmail.com>
2024-05-08 00:35:36 +0600

    database migration added

---
+++ database/migrations/2024_05_07_182832_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->enum('transaction_type', ['Deposit', 'Withdrawal']);
+            $table->double('amount', 10, 2)->nullable();
+            $table->decimal('fee', 10, 2)->nullable();
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
